81b82d214dc468d4aba5bfc6b4c4bb61e9c373d9
Nicholas Skinner <njskinner@ucdavis.edu>
2022-11-30 19:14:30 -0800

    header sidebar created and formatted, posts instatiated

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,12 +1,14 @@
 import "./header.css"
+import Noraltaa from "../../src/img/noraltaa.jpg"
 
 export default function Header() {
   return (
     <div className="header">
         <div className="headerTitles">
-            <span>React & Node</span>
-            <span>Blog</span>
+            <span className="headerTitleSm">REACT & NODE</span>
+            <span className="headerTitleLg">blog</span>
         </div>
+        <img className="headerImg" src={Noraltaa} alt="" />
     </div>
   )
 }
---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,34 @@
+import "./sidebar.css"
+import AboutImg from "../../src/img/aboutMe.JPG"
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+        <div className="sidebarItem">
+            <span className="sidebarTitle">about me</span>
+            {/* <img src={AboutImg} alt="" /> */}
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi nihil sunt voluptate distinctio exercitationem consectetur,</p>
+        </div>
+        <div className="sidebarItem">
+            <span className="sidebarTitle">categories</span>
+            <ul className="sidebarList">
+                <li className="sidebarListItem">Life</li>
+                <li className="sidebarListItem">Music</li>
+                <li className="sidebarListItem">Games</li>
+                <li className="sidebarListItem">Stuff</li>
+                <li className="sidebarListItem">Other Stuff</li>
+                <li className="sidebarListItem">More Stuff</li>
+            </ul>
+        </div>
+        <div className="sidebarItem">
+            <span className="sidebarTitle">follow</span>
+            <div className="sidebarSocial">
+                <i className="sidebarIcon fa-brands fa-square-tumblr"></i>
+                <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+                <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+                <i className="sidebarIcon fa-solid fa-square-envelope"></i>
+            </div>
+        </div>
+    </div>
+  )
+}

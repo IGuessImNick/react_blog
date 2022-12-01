f3226b4a178a8c90f94e7ca6c6464a3d1d8fc4b8
Nicholas Skinner <njskinner@ucdavis.edu>
2022-11-30 16:49:56 -0800

    topbar created

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,28 @@
+import "./topbar.css";
+import Pfp from "../../src/img/pfp.JPG"
+
+export default function TopBar() {
+  return (
+    <div className='top'>
+        <div className="topLeft">
+            <i className="topIcon fa-brands fa-square-tumblr"></i>
+            <i className="topIcon fa-brands fa-square-instagram"></i>
+            <i className="topIcon fa-brands fa-square-pinterest"></i>
+            <i className="topIcon fa-solid fa-square-envelope"></i>
+        </div>
+        <div className="topCenter">
+            <ul className="topList">
+                <li className="topListItem">HOME</li>
+                <li className="topListItem">ABOUT</li>
+                <li className="topListItem">CONTACT</li>
+                <li className="topListItem">WRITE</li>
+                <li className="topListItem">LOGOUT</li>
+            </ul>
+        </div>
+        <div className="topRight">
+            <img className="topImg" src={Pfp} alt="" />
+            <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+        </div>
+    </div>
+  )
+}

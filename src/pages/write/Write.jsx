ae7dc569a213d2fe12262cb0f85f06a1a95b3311
Nicholas Skinner <njskinner@ucdavis.edu>
2022-12-01 10:15:11 -0800

    write page created and formatted

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -3,7 +3,21 @@ import "./write.css"
 export default function Write() {
   return (
     <div className="write">
-
+        <img className="writeImg" src="" alt="" />
+        <form className="writeForm">
+            <div className="writeFormGroup">
+                <label htmlFor="fileInput">
+                    <i className="writeIcon fa-solid fa-plus"></i>
+                </label>
+                <input type="file" id="fileInput" style={{display:"none"}}/>
+                {/* autoFocus = {true} will force page to focus on element on page open/refresh */}
+                <input type="text" placeholder="Title" className="writeInput" autoFocus={true} />
+            </div>
+            <div className="writeFormGroup">
+                <textarea placeholder="What's on your mind?" type="text" className="writeInput writeText"></textarea>
+            </div>
+            <button className="writeSubmit">Publish</button>
+        </form>
     </div>
   )
 }
